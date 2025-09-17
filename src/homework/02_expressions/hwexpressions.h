0b609fd5cbba2e8b5ad9fb9087912be9315e3808
Tyler Lane <144444269+Tylerdlane@users.noreply.github.com>
2025-09-17 23:58:30 +0000

    Completed Homework 02

--- src/homework/02_expressions/hwexpressions.h
+++ src/homework/02_expressions/hwexpressions.h
@@ -1,7 +1,9 @@
-﻿//example
-int add_numbers(int num1, int num2);
+﻿// Constant for tax rate
+const double tax_rate = 0.0675;
 
-//write function prototype here
+// Function declarations
+double get_sales_tax_amount(double meal_amount);
+double get_tip_amount(double meal_amount, double tip_rate);
 
 
 
--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -1,11 +1,29 @@
-﻿//write include statements
+﻿#include <iostream>
+#include <iomanip>
+#include "hwexpressions.h"
 
+using namespace std;
 
-//write namespace using statement for cout
+int main() {
+    double meal_amount, tip_rate;
 
-int main()
-{
-	
+    cout << "Enter the meal amount: $";
+    cin >> meal_amount;
 
-	return 0;
+    cout << "Enter the tip rate (e.g., 0.15 for 15%): ";
+    cin >> tip_rate;
+
+    double tax_amount = get_sales_tax_amount(meal_amount);
+    double tip_amount = get_tip_amount(meal_amount, tip_rate);
+    double total = meal_amount + tax_amount + tip_amount;
+
+    cout << fixed << setprecision(2); // Format to 2 decimal places
+
+    cout << "\nReceipt:\n";
+    cout << "Meal Amount:     $" << meal_amount << endl;
+    cout << "Sales Tax:       $" << tax_amount << endl;
+    cout << "Tip Amount:      $" << tip_amount << endl;
+    cout << "Total:           $" << total << endl;
+
+    return 0;
 }
