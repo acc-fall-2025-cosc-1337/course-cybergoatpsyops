a6267f565421cb415661b2e0abc0b087187eab20
Aisha S Campos <aisha.campos@g.austincc.edu>
2025-09-20 00:44:37 +0000

    adding tax and total

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -3,18 +3,23 @@
 using namespace std;
 
 
-double calculate_tax_from_input()
+int main()
 {
-	double meal_cost, tip;
+	double meal_cost, tip, tax, total;
 	std::cout << "Enter meal cost:";
-	std::cin >> total;
+	std::cin >> meal_cost
 	std::cout << "Enter tip amount:";
 	std::cin >> tip;
 
 	const double tax_rate = 6.75/100;
-	double tax =(total - tip) * tax_rate;
+	double tax =(meal_cost - tip) * tax_rate;
 	std::cout <<"Calculated tax:" << tax_rate <<std::endl;
 	return tax;
+
+	cout<<"Total for your meal is:"<< total << endl;
+	return 0;
+	
+
 }
 
 //write function code here
