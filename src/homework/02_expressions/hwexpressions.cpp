d93153b7ac94f68744c6f35780a39e10eac9b736
Aisha S Campos <aisha.campos@g.austincc.edu>
2025-09-20 01:38:24 +0000

    changing variable name

--- src/homework/02_expressions/hwexpressions.cpp
+++ src/homework/02_expressions/hwexpressions.cpp
@@ -5,21 +5,21 @@ using namespace std;
 
 int main()
 {
-	double meal_cost, tip, total;
+	double meal_amount, get_tip_amount, total;
 	std::cout << "Enter meal cost:";
-	std::cin >> meal_cost
+	std::cin >> meal_amount;
 	std::cout << "Enter tip amount:";
-	std::cin >> tip;
+	std::cin >> get_tip_amount;
 
 	const double tax_rate = 6.75/100;
-	double tax =(meal_cost - tip) * tax_rate;
+	double tax =(meal_amount - get_tip_amount) * tax_rate;
 	std::cout <<"Calculated tax:" << tax_rate <<std::endl;
 	return tax_rate;
 
-	double tip= meal_cost * tip;
-	double total = meal_cost + tip + tax;
+	double tip= meal_amount * get_tip_amount;
+	double total = meal_cost + get_tip_amount + tax;
 
-	std:: cout << "Meal cost: $" << meal_cost <<std::endl;
+	std:: cout << "Meal cost: $" << meal_amount <<std::endl;
 	std::cout <<"Tip: $" << tip <<std::endl;
 	std::cout <<"Tax: $" << tax <<std::endl;
 	std::cout<<"Total: $" << total <<std::endl;
