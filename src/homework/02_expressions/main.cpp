8f08da8eceda4ffbfaad1c61e66b4f2ac9b59175
DuncanH26 <duncan.hemelstrand@g.austincc.edu>
2025-09-24 22:10:12 +0000

    hw 02

--- src/homework/02_expressions/main.cpp
+++ src/homework/02_expressions/main.cpp
@@ -5,7 +5,18 @@
 
 int main()
 {
-	
+	double meal_cost = 50;
+	double tip_percentage = .2;
+
+	double sales_tax = get_sales_tax_amount(meal_cost);
+	double tip_amount = get_tip_amount(meal_cost, tip_percentage);
+
+	std::cout << "Meal cost: $" << meal_cost << std::endl;
+	std::cout << "Sales tax: $" << sales_tax << std::endl;
+	std::cout << "Tip amount: $" << tip_amount << std::endl;
+
+	double total_bill  = meal_cost + sales_tax + tip_amount;
+	std::cout << "Total bill: $" << total_bill << std::endl;
 
 	return 0;
 }
--- test/homework/02_expressions/02_expressions_tests.cpp
+++ test/homework/02_expressions/02_expressions_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "hwexpressions.h"
+#include "hwexpressions.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -10,5 +11,20 @@ TEST_CASE("Verify sum_numbers function")
 {
 	REQUIRE(add_numbers(5, 5) == 10);
 	REQUIRE(add_numbers(10, 10) == 20);
+	
 }
 
+TEST_CASE("Verify get_sales_tax_amount function")
+{
+
+	REQUIRE(get_sales_tax_amount(10) == .675);
+	REQUIRE(get_sales_tax_amount(20) == 1.35);
+
+}
+
+TEST_CASE("Verify get_tip_amount function")
+{
+
+	REQUIRE(get_tip_amount(20, .15) == 3);
+	REQUIRE(get_tip_amount(20, .2) == 4);
+} 
