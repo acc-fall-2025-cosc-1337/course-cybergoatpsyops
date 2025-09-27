de11320ba40adf5ae585155973056ede896b31ff
graybeasley <grayton.beasley@g.austincc.edu>
2025-09-27 21:22:20 +0000

    Homework 3: Creating functions, variables, input/output, and conditional structures.

--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,47 @@
 //write include statements
+#include <iostream>
+#include "decisions.h"
 
-int main() 
+//main program
+
+int main()
 {
-	return 0;
+    int choice;
+    int grade;
+
+    std::cout << "MAIN MENU\n";
+    std::cout << "1 - Letter grade using if\n";
+    std::cout << "2 - Letter grade using switch\n";
+    std::cout << "3 - Exit\n";
+    std::cout << "Enter choice: ";
+    std::cin >> choice;
+
+    if (choice == 1)
+    {
+        std::cout << "Enter a numerical grade (0-100): ";
+        std::cin >> grade;
+        if (grade >= 0 && grade <= 100)
+            std::cout << "Letter grade: " << get_letter_grade_using_if(grade) << "\n";
+        else
+            std::cout << "Number out of range.\n";
+    }
+    else if (choice == 2)
+    {
+        std::cout << "Enter a numerical grade (0-100): ";
+        std::cin >> grade;
+        if (grade >= 0 && grade <= 100)
+            std::cout << "Letter grade: " << get_letter_grade_using_switch(grade) << "\n";
+        else
+            std::cout << "Number out of range.\n";
+    }
+    else if (choice == 3)
+    {
+        std::cout << "Exiting...\n";
+    }
+    else
+    {
+        std::cout << "Invalid option.\n";
+    }
+
+    return 0;
 }
