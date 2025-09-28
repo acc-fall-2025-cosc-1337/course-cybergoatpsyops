4a870164c6c034b9d1a4184bb05255076df6520d
Aisha S Campos <aisha.campos@g.austincc.edu>
2025-09-28 03:00:00 +0000

    headers and test cases

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,37 @@
 //write include statement for decisions header
+#include "if_else_if.h"
+#include "switch.h"
+
+using std::string;
+
+string get_letter_grade_using_if(int grade)
+{
+    //Write code for function(s) code here
+    if (grade >= 90 && grade <= 100)
+    {
+        return "A";
+    }
+    else if (grade >= 80 && grade <= 89)
+    {
+        return "B";
+    }
+    else if (grade >= 70 && grade <= 79)
+    {
+        return "C";
+    }
+    else if (grade >= 60 && grade <= 69)
+    {
+        return "D";
+    }
+    else if (grade >= 0 && grade <= 59)
+    {
+        return "F";
+    }
+    else
+    {
+        return "Invalid grade";
+    }
+}
 
 
 //Write code for function(s) code here
