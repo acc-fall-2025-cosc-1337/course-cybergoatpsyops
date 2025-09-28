9b828f30c68b54d1cb71c8189a7f27a169c45404
Aisha S Campos <aisha.campos@g.austincc.edu>
2025-09-28 03:18:57 +0000

    Adding switch and break

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -34,3 +34,29 @@ string get_letter_grade_using_if(int grade)
         return "Invalid grade";
     }
 }
+
+string get_letter_grade_using_switch(int grade)
+{
+    if (grade < 0 || grade > 100)
+        return "Invalid grade";
+
+    switch (grade / 10)
+    {
+        case 10:
+        case 9:
+            return "A";
+            break;
+        case 8:
+            return "B";
+            break;
+        case 7:
+            return "C";
+            break;
+        case 6:
+            return "D";
+            break;
+        default:
+            return "F";
+            break;
+    }
+}
