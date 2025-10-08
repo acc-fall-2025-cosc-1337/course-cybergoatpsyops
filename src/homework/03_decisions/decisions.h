becbbce91749efbce541b76bbf0f08706a049341
DuncanH26 <duncan.hemelstrand@g.austincc.edu>
2025-10-08 22:22:07 +0000

    hw 03

--- src/homework/03_decisions/decisions.h
+++ src/homework/03_decisions/decisions.h
@@ -1,7 +1,12 @@
 //write include required statements
-
+#define decisions_h
+#include <string>
 
 //Write a function(s) prototype  here
+
+char get_letter_grade_using_if(int grade);
+
+char get_letter_grade_using_switch(int grade);
 
 
 
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,53 @@
 //write include statements
-
+#include <iostream>
+#include "decisions.h"
 int main() 
 {
+	int choice;
+	int grade;
+
+	std::cout << '1-Letter grade using if';
+	std::cout << '2-Letter grade using switch';
+	std::cout << '3-Exit'
+	std::cout << 'Enter an option: ';
+	std::cin >> choice;
+
+
+	if (choice == 1) {
+
+		std::cout << 'Enter grade: '
+		std::cin >> grade;
+
+		if (grade >= 0 && grade <= 100) {
+			 letter_grade = get_letter_grade_using_if(grade);
+			std::cout << 'Letter grade (using if): ' << letter_grade << "/n";
+			 
+		} else if {
+
+			(choice == 2)
+			letter_grade == get_letter_grade_using_switch(grade);
+			std::cout << 'Letter grade (using switch): ' << letter_grade << "\n";
+		} 
+			
+			
+			}
+			else {
+
+				std::cout << "Invalid number";
+			}
+           
+		
+	} else if (choice == 3)
+	{
+		std::cout 'Proogram Exiting...';
+	}
+
+	else {
+		std::cout 'Invalid option'
+	}
+
+	
+
+
 	return 0;
 }
