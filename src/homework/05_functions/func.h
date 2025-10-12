27c2c04bf1e2ca6023e025bdc578390c1a41976f
graybeasley <grayton.beasley@g.austincc.edu>
2025-10-12 16:20:21 +0000

    HW05: DNA Functions

--- src/homework/05_functions/func.h
+++ src/homework/05_functions/func.h
@@ -1,1 +1,14 @@
 //add function(s) prototype here
+#ifndef FUNC_H
+#define FUNC_H
+
+#include <string>
+using std::string;
+
+double get_gc_content(const string& dna);
+
+string get_reverse_string(string dna);
+
+string get_dna_complement(string dna);
+
+#endif
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,42 @@
-
-int main() 
+#include "func.h"
+#include <iostream>
+using namespace std;
+//main
+int main()
 {
+    int choice;
+    string dna;
 
-	return 0;
+    do
+    {
+        cout << "\nDNA Menu\n";
+        cout << "1 - Get GC Content\n";
+        cout << "2 - Get DNA Complement\n";
+        cout << "3 - Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            cout << "Enter DNA string: ";
+            cin >> dna;
+            double gc = get_gc_content(dna);
+            cout << "GC Content: " << gc << endl;
+        }
+        else if (choice == 2)
+        {
+            cout << "Enter DNA string: ";
+            cin >> dna;
+            string complement = get_dna_complement(dna);
+            cout << "DNA Complement: " << complement << endl;
+        }
+        else if (choice != 3)
+        {
+            cout << "Invalid choice. Try again.\n";
+        }
+
+    } while (choice != 3);
+
+    cout << "Program exited.\n";
+    return 0;
 }
